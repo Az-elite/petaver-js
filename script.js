b7d1cc0646056cc09a45399ac37a2f12aa380da9
Akolade-Azeez <akoladeabiodun@gmail.com>
2024-07-19 01:23:42 +0100

    petaverse-js
    for petavers js only

---
+++ script.js
@@ -0,0 +1,38 @@
+// function greet(timeOfDay, morningMessage, afternoonMessage, nightMessag) {
+//   if (timeOfDay === "morning") {
+    // return morningMessage;
+//   } else if (timeOfDay === "afternoon") {
+    // return afternoonMessage;
+//   } else if (timeOfDay === "night") {
+    // return nightMessag;
+//   } else {
+    // return "hello";
+//   }
+// }
+// 
+// // console.log(greet("morning", "goodmorning", "goodafternnon", "goodnight!"));
+// // console.log(greet("afternoon", "goodmorning", "goodafternnon", "goodnight!"));
+// // console.log(greet("goodnight", "goodmorning", "goodafternnon", "goodnight!"));
+// // console.log(greet('evening', 'Good morning', 'Good afternoon', 'Good night!'));
+
+
+
+
+// const addnum = (num1 , num2) => {
+// let sum = num1 + num2
+// console.log(sum);
+// }
+// 
+// addnum(14,78)
+// addnum(10,2)
+// addnum(100,4)
+// 
+
+const name = "Akolade Azeez"
+let greeting = "Good morning how are you doing today"
+let complexion = "Chololate"
+let age = 25
+
+console.log(` ${name}\n${greeting}\n ${complexion}\n${age}`);
+
+
